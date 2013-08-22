6853e32d902521b0a8fc6ceb84bf5e665a491964
remcotolsma <remcotolsma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-22 12:40:34 +0000

    tagging version 0.2.1
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-events/tags/0.2.1@760637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pronamic-events.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Pronamic Events
+Plugin URI: http://pronamic.eu/wordpress/events/
+Description: This plugin add some basic Event functionality to WordPress
+
+Version: 0.2.1
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://pronamic.eu/
+
+Text Domain: pronamic_events
+Domain Path: /languages/
+
+License: GPL
+
+GitHub URI: https://github.com/pronamic/wp-pronamic-events
+*/
+
+require_once dirname( __FILE__ ) . '/classes/Pronamic_Events_Plugin.php';
+require_once dirname( __FILE__ ) . '/classes/Pronamic_Events_Widget.php';
+require_once dirname( __FILE__ ) . '/classes/Pronamic_Events_Plugin_Admin.php';
+
+global $pronamic_events_plugin;
+
+$pronamic_events_plugin = new Pronamic_Events_Plugin( __FILE__ );
